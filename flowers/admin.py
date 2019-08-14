7f1d20d85423540af2904e08ba08ee4580ef5065
feven <feven.m.woldu@gmail.com>
2019-08-14 14:34:00 +0300

    Created a superuser and registered my models in the admin.py file.

--- flowers/admin.py
+++ flowers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Seller, Inventory
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Seller)
+admin.site.register(Inventory)
