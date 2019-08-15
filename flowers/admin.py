3096fd3a04e319bd90e553f9adc36f06150dcc8e
feven <feven.m.woldu@gmail.com>
2019-08-15 23:19:15 +0300

    Changed the customer with the customuser and the seller to vendor.

--- flowers/admin.py
+++ flowers/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Seller, Inventory
+from .models import CustomUser,Vendor,Inventory
 
 # Register your models here.
 
-admin.site.register(Customer)
-admin.site.register(Seller)
+admin.site.register(CustomUser)
+admin.site.register(Vendor)
 admin.site.register(Inventory)
